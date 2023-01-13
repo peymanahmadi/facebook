ab0fb665b96894233d84e6435e52da86e818ce64
peymanahmadi <peyman.ahmadi@outlook.com>
2023-01-13 22:38:46 +0330

    Setup project

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to facebook");
-});
+// routes
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.listen(8000, () => {
-  console.log("Server is listening...");
+// database
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
 });
